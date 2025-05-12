814b8e4f51e539e9be272fe1bf7a81bf2f3da636
JinhyeonK <diamondataJH@gmail.com>
2025-05-12 14:09:16 +0900

    Title: 변수 선언하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250512/변수 선언하기/declaring-variables.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a=b;
+        a=3;
+        b="C";
+        System.out.pritnln(a);
+        System.out.pritnln(b);
+    }
+}
